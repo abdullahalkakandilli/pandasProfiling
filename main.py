1a60a2ff1b00e331ada86ab78d6e8963ad545acf
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-02-27 09:54:26 +0100

    ui update

--- main.py
+++ main.py
@@ -64,5 +64,5 @@ else:
             👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
             """
     )
-    st.video('https://youtu.be/5rACpMjHBN0')
+    st.video('https://youtu.be/HgLyNo0UGXo')
     st.stop()
